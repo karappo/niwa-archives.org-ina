02f16b6a2730c68a7427845a2d8fa9ff52f379a8
Naokazu Terada <naokazu.terada@gmail.com>
2020-11-16 19:24:45 +0900

    Modify rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,17 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
-  plugins: ['prettier'],
+  plugins: [
+    'prettier'
+  ],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'prettier/prettier': ['error', {'trailingComma': 'none'}],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-irregular-whitespace': 'off',
+    'vue/name-property-casing': ['error', 'kebab-case'],
+    'yoda': ['error', 'never', {'onlyEquality': true}],
+    'unicorn/number-literal-case': 'off'
+  }
 }
