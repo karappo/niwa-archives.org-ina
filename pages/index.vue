6e1754102cf83f1b4d996958c77a66fdf0503cad
Naokazu Terada <naokazu.terada@gmail.com>
2020-11-17 23:07:47 +0900

    トップページ

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-div
+div 上のタブを押してください
 </template>
 
 <style lang="sass" scoped>
@@ -7,4 +7,10 @@ div
   background: black
   width: 100%
   height: 100%
+  display: flex
+  justify-content: center
+  align-items: center
+  text-align: center
+  font-size: 25px
+  color: white
 </style>
