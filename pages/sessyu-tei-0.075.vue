be39ac6c148379fb98eed96430c18288f0521a49
Naokazu Terada <naokazu.terada@gmail.com>
2020-11-17 18:03:50 +0900

    Add sessyu-tei 0.08, 0.075

---
+++ pages/sessyu-tei-0.075.vue
@@ -0,0 +1,3 @@
+<template lang="pug">
+Viewer(file='/pointclouds/sessyu-tei-0.075/metadata.json')
+</template>
---
+++ pages/sessyu-tei-0.08.vue
@@ -0,0 +1,3 @@
+<template lang="pug">
+Viewer(file='/pointclouds/sessyu-tei-0.08/metadata.json')
+</template>
