2ddd3ace625c88b928d0214ae7cd8c1b517c6142
Naokazu Terada <naokazu.terada@gmail.com>
2021-03-18 10:58:47 +0900

    Move styles to global

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,3 @@
 #pageTop
   nuxt/
 </template>
-
-<style lang="sass" scoped>
-#pageTop
-  height: 100vh
-  display: flex
-  flex-direction: column
-</style>
-
-<script>
-export default {}
-</script>
