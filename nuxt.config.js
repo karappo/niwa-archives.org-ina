d2c7ae0b0f02ad5bdb430d9be75c2678bc841d95
Naokazu Terada <naokazu.terada@gmail.com>
2021-03-19 11:06:11 +0900

    Use @nuxt/content

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,9 +41,8 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    // https://go.nuxtjs.dev/axios
+    '@nuxt/content',
     '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
   ],
 
