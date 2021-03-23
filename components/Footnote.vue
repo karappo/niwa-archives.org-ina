82e82b269d0293e4cb2fa97ac354dd534f4d1870
Naokazu Terada <naokazu.terada@gmail.com>
2021-03-23 13:23:06 +0900

    Update content

---
+++ components/Footnote.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+sup.footnote-ref
+  a(:href="'#fn'+num" :id="'fnref'+num") {{ num }}
+</template>
+
+<script>
+export default {
+  props: {
+    num: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
