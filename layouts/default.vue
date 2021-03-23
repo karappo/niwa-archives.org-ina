72dced92746ef66e8d2b9c8c05e2d78c5cc48620
Naokazu Terada <naokazu.terada@gmail.com>
2021-03-23 10:58:24 +0900

    wip: Add japanese page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,68 +1,63 @@
 <template lang="pug">
 #pageTop
   header
-    .colL
-      h1
-        Img2X.logo(src='logo.png')
-    .colR
-      .label
-        span.ja 終らない庭のアーカイヴ
-        span.en Project Outline
-      nav.lang
-        nuxt-link(to='/en/' :class="{current: $nuxt.$route.path === '/en/'}") EN
-        nuxt-link(to='/' :class="{current: $nuxt.$route.path === '/'}") JA
+    h1
+      Img2X.logo(src='logo.png')
+      span Project Outline
+    nav.sections
+      a(href="#about") About
+      a(href="#report") Report
+      a(href="#credit") Credit
+    nav.languages
+      nuxt-link(to='/en/' :class="{current: $nuxt.$route.path === '/en/'}") English
+      nuxt-link(to='/' :class="{current: $nuxt.$route.path === '/'}") Japanese
   nuxt/
 </template>
 
 <style lang="sass" scoped>
 @import '~/assets/style/general/const'
 #pageTop
-  padding: 62px
   header
+    padding: 88px 62px
+    margin-top: -21px
+    margin-left: 5px
+    font-family: $helvetica
     display: flex
-    margin-top: 5px
-    .colL
-      width: auto
-      flex: 1
-      .logo
-        position: fixed
-    .colR
-      width: 690px
-      margin-right: 0
+    h1
+      position: fixed
+      display: flex
+      align-items: flex-start
+      margin-top: 1px
+      span
+        color: #A3A3A3
+        font-size: 15px
+        font-weight: lighter
+        letter-spacing: 0.05em
+        margin-top: 2px
+        margin-left: 70px
+    nav
+      a
+        font-size: 15px
+        letter-spacing: 0.06em
+        padding-bottom: 13px
+      .current
+        border-bottom: 1px solid #A9BCB6
+      a + a
+        margin-left: 32px
+    nav.sections
+      margin: 3px 102px 0 auto
+    nav.languages
+      margin: 3px 0 0 0
       display: flex
       position: relative
-      .label
-        position: relative
-        display: flex
-        flex-direction: column
-        margin-top: -10px
-        &:before
-          position: absolute
-          top: 11px
-          left: -128px
-          content: ''
-          display: block
-          width: 110px
-          border-top: 1px solid #516760
-        .ja
-          font-size: 16px
-        .en
-          margin-top: 14px
-          font-size: 14px
-      nav.lang
-        position: fixed
-        margin-top: 3px
-        right: 63px
-        display: flex
-        font-family: $helvetica
-        a
-          font-size: 15px
-          letter-spacing: 0.03em
-          padding-bottom: 13px
-        .current
-          border-bottom: 1px solid #A9BCB6
-        a + a
-          margin-left: 1em
+      &:before
+        content: ''
+        position: absolute
+        top: 7px
+        left: -90px
+        display: block
+        width: 70px
+        border-top: 1px solid #516760
 </style>
 
 <script>
