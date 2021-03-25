f642a4d701640eab337f3f7bc89e72bf3c985857
Naokazu Terada <naokazu.terada@gmail.com>
2021-03-25 16:18:50 +0900

    Update description

--- assets/messages.js
+++ assets/messages.js
@@ -1,8 +1,8 @@
 
 module.exports = {
   ja: {
-    title: '終わらない庭のアーカイヴ - Incomplete Niwa Archives',
-    description: 'TODO 説明',
+    title: '終わらない庭のアーカイヴ',
+    description: '2019年4月より始動した庭園アーカイヴ・プロジェクトは、現代のテクノロジーを駆使して、日本庭園の多様な側面をデータ化した新しい総合的アーカイヴを研究開発するものです。',
     carouselCaptions: [
       '常栄寺庭園の3Dスキャン',
       '常栄寺庭園の3Dスキャン',
@@ -11,8 +11,8 @@ module.exports = {
     ]
   },
   en: {
-    title: 'Incomplete Niwa Archives - 終わらない庭のアーカイヴ',
-    description: 'TODO 説明',
+    title: 'Incomplete Niwa Archives',
+    description: 'Launched in April 2019, the Garden Archives Project aims to study and develop a new kind of comprehensive archives that digitally survey Japanese gardens’ diverse aspects.',
     carouselCaptions: [
       '3D scans of Jōei-ji Garden',
       '3D scans of Jōei-ji Garden',
