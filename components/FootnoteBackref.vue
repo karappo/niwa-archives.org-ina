cd873b532002ba1c44562e22fecfcb5aa1d39dda
Naokazu Terada <naokazu.terada@gmail.com>
2021-03-26 12:08:13 +0900

    Convert '↩' to svg for avoiding emoji rendering on iOS

--- components/FootnoteBackref.vue
+++ components/FootnoteBackref.vue
@@ -1,6 +1,16 @@
 <template lang="pug">
-a.footnote-backref(v-scroll-to="{el: `#fnref${num}`, offset: 0}" href="#") ↩
+a.footnote-backref(v-scroll-to="{el: `#fnref${num}`, offset: 0}" href="#")
+  img.arrow(src="~/assets/image/back-arrow.svg")
 </template>
+
+<style lang="sass" scoped>
+.arrow
+  display: inline-block
+  height: 0.7em
+  width: auto
+  margin-left: 0.5em
+  vertical-align: middle
+</style>
 
 <script>
 export default {
