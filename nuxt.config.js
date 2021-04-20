d342063aae210a1ec9a31bf53ea22697b3883bad
Naokazu Terada <naokazu.terada@gmail.com>
2021-04-20 21:13:51 +0900

    Clean up

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,9 +51,7 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
   ],
 
