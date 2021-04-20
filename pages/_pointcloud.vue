b1c0ecb3b29870d47c15d97794c298ae4f6f4e2d
Naokazu Terada <naokazu.terada@gmail.com>
2021-04-20 23:09:21 +0900

    Add animation

--- pages/_pointcloud.vue
+++ pages/_pointcloud.vue
@@ -25,16 +25,17 @@ export default {
     }
   },
   async mounted() {
-    window.viewer = new Potree.Viewer(this.$refs.potree_render_area)
-    window.viewer.setFOV(60)
-    window.viewer.setPointBudget(2_000_000)
-    window.viewer.loadSettingsFromURL()
+    const viewer = new Potree.Viewer(this.$refs.potree_render_area)
+    window.viewer = viewer
+    viewer.setFOV(60)
+    viewer.setPointBudget(2_000_000)
+    viewer.loadSettingsFromURL()
 
-    window.viewer.loadGUI(() => {
-      window.viewer.setLanguage('en')
+    viewer.loadGUI(() => {
+      viewer.setLanguage('en')
       $('#menu_tools').next().show()
       $('#menu_scene').next().show()
-      window.viewer.toggleSidebar()
+      viewer.toggleSidebar()
     })
 
     const file = `/pointclouds/${this.$route.params.pointcloud}/metadata.json`
@@ -44,31 +45,51 @@ export default {
     material.pointSizeType = Potree.PointSizeType.ADAPTIVE
 
     const config = () => {
-      window.viewer.setEDLEnabled(this.$store.state.EDLEnabled)
-      window.viewer.setEDLRadius(this.$store.state.EDLRadius)
-      window.viewer.setEDLStrength(this.$store.state.EDLStrength)
-      window.viewer.setEDLOpacity(this.$store.state.EDLOpacity)
+      viewer.setEDLEnabled(this.$store.state.EDLEnabled)
+      viewer.setEDLRadius(this.$store.state.EDLRadius)
+      viewer.setEDLStrength(this.$store.state.EDLStrength)
+      viewer.setEDLOpacity(this.$store.state.EDLOpacity)
       material.shape = this.$store.state.shape
       material.size = this.$store.state.size
     }
 
-    window.viewer.scene.addPointCloud(pointcloud)
+    viewer.scene.addPointCloud(pointcloud)
 
     if (/sessyu-tei-.*/.test(this.$route.params.pointcloud)) {
       const images = await Potree.OrientedImageLoader.load(
         '/orientedImage/cameraParams.xml',
         '/orientedImage/imageParams.txt',
-        window.viewer
+        viewer
       )
-      window.viewer.scene.addOrientedImages(images)
+      viewer.scene.addOrientedImages(images)
 
-      // window.viewer.fitToScreen()
-      window.viewer.scene.view.position.set(-41.287, 31.136, -0.427)
-      window.viewer.scene.view.lookAt(new THREE.Vector3(-34.27, 25.369, -2.105))
+      // viewer.fitToScreen()
+      viewer.scene.view.position.set(-45.509, 48.096, 26.978)
+      viewer.scene.view.lookAt(new THREE.Vector3(-34.27, 25.369, -2.105))
+
+      const animation = new Potree.CameraAnimation(viewer)
+      const positions = [
+        [-45.509, 48.096, 26.978],
+        [-45.705, 13.655, -1.111],
+        [-43.002, 7.955, -1.188],
+        [5.121, -6.388, 10.691]
+      ]
+      const targets = [
+        [-34.27, 25.369, -2.105],
+        [-44.209, 10.512, -2.616],
+        [-41.335, 7.212, -1.703],
+        [-23.185, 22.343, -13.972]
+      ]
+      for (let i = 0; i < positions.length; i++) {
+        const cp = animation.createControlPoint()
+        cp.position.set(...positions[i])
+        cp.target.set(...targets[i])
+      }
+      viewer.scene.addCameraAnimation(animation)
     } else {
-      // window.viewer.fitToScreen()
-      window.viewer.scene.view.position.set(-8.967, 18.538, -1.725)
-      window.viewer.scene.view.lookAt(new THREE.Vector3(-4.973, 12.468, -1.986))
+      // viewer.fitToScreen()
+      viewer.scene.view.position.set(-13.397, 8.883, 27.045)
+      viewer.scene.view.lookAt(new THREE.Vector3(-10.916, 6.771, -2.138))
     }
 
     this.$nuxt.$on('settingUpdated', config)
