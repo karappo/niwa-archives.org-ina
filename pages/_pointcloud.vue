3ccd6a5732fa72a844f5e99f72624b2d2e903be8
Naokazu Terada <naokazu.terada@gmail.com>
2021-04-20 22:09:58 +0900

    sidebar上での操作でエラーが出るので、viewerをwindowに生やす

--- pages/_pointcloud.vue
+++ pages/_pointcloud.vue
@@ -25,16 +25,16 @@ export default {
     }
   },
   async mounted() {
-    const viewer = new Potree.Viewer(this.$refs.potree_render_area)
-    viewer.setFOV(60)
-    viewer.setPointBudget(2_000_000)
-    viewer.loadSettingsFromURL()
+    window.viewer = new Potree.Viewer(this.$refs.potree_render_area)
+    window.viewer.setFOV(60)
+    window.viewer.setPointBudget(2_000_000)
+    window.viewer.loadSettingsFromURL()
 
-    viewer.loadGUI(() => {
-      viewer.setLanguage('en')
+    window.viewer.loadGUI(() => {
+      window.viewer.setLanguage('en')
       $('#menu_tools').next().show()
       $('#menu_scene').next().show()
-      viewer.toggleSidebar()
+      window.viewer.toggleSidebar()
     })
 
     const file = `/pointclouds/${this.$route.params.pointcloud}/metadata.json`
@@ -44,24 +44,24 @@ export default {
     material.pointSizeType = Potree.PointSizeType.ADAPTIVE
 
     const config = () => {
-      viewer.setEDLEnabled(this.$store.state.EDLEnabled)
-      viewer.setEDLRadius(this.$store.state.EDLRadius)
-      viewer.setEDLStrength(this.$store.state.EDLStrength)
-      viewer.setEDLOpacity(this.$store.state.EDLOpacity)
+      window.viewer.setEDLEnabled(this.$store.state.EDLEnabled)
+      window.viewer.setEDLRadius(this.$store.state.EDLRadius)
+      window.viewer.setEDLStrength(this.$store.state.EDLStrength)
+      window.viewer.setEDLOpacity(this.$store.state.EDLOpacity)
       material.shape = this.$store.state.shape
       material.size = this.$store.state.size
     }
 
-    viewer.scene.addPointCloud(pointcloud)
-    viewer.fitToScreen()
+    window.viewer.scene.addPointCloud(pointcloud)
+    window.viewer.fitToScreen()
 
     if (/sessyu-tei-.*/.test(this.$route.params.pointcloud)) {
       const images = await Potree.OrientedImageLoader.load(
         '/orientedImage/cameraParams.xml',
         '/orientedImage/imageParams.txt',
-        viewer
+        window.viewer
       )
-      viewer.scene.addOrientedImages(images)
+      window.viewer.scene.addOrientedImages(images)
     }
 
     this.$nuxt.$on('settingUpdated', config)
