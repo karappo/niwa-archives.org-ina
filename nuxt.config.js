2a427424e24abba3825632b2642961d11f5945b5
Naokazu Terada <naokazu.terada@gmail.com>
2021-04-21 18:45:19 +0900

    Change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'niwa-archive-nuxt',
+    title: 'Incomplete Niwa Archives 終らない庭のアーカイヴ',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
