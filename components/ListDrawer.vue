3c3eb9c2f199d21cf84c5fa1361f5f6943f12c11
Naokazu Terada <naokazu.terada@gmail.com>
2021-08-23 01:08:25 +0900

    リストにサムネイルとファイルタイプのアイコンを表示

--- components/ListDrawer.vue
+++ components/ListDrawer.vue
@@ -9,8 +9,10 @@ article
     li(v-for="o in data.list" @click="$emit('showAnnotation', o.index)")
       Icon(v-if="!icon" :category="o.category" :data-hoge="o.youtube.thumbnailUrl()")
       .thumb(v-if="o.image" :style="`background-image: url(${o.image});`")
-      .thumb(v-else-if="o.youtube" :style="`background-image: url(${o.youtube.thumbnailUrl()});`")
-      span {{ o.title }}
+      .thumb(v-if="o.youtube" :style="`background-image: url(${o.youtube.thumbnailUrl()});`")
+      span.title {{ o.title }}
+      span.type(v-if="o.image") 
+      span.type.youtube(v-else-if="o.youtube") 
   .empty(v-else) データがありません
 </template>
 
@@ -57,17 +59,29 @@ h1
     border-top: 1px solid #555
     color: #898989
     cursor: pointer
+    font-size: 15px
     &:hover
       color: white
     .icon
       margin-right: 0.5em
     .thumb
       width: 40px
-      height: 40px
+      height: 30px
       border-radius: 5px
       background-size: cover
       background-position: center
       margin-right: 10px
+    .title
+      overflow: hidden
+      text-overflow: ellipsis
+      white-space: nowrap
+    .type
+      font-size: 18px
+      margin-left: auto
+      margin-right: 0
+      font-family: 'Font Awesome 5 Pro-Light-300'
+      &.youtube
+        font-family: 'Font Awesome 5 Brands-Regular-400'
   li:last-child
     border-bottom: 1px solid #555
 .empty
