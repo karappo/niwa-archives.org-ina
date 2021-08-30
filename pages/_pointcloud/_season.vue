4e639531d2b2a7fb6c3573b62d4e39e814d12277
Naokazu Terada <naokazu.terada@gmail.com>
2021-08-30 18:58:44 +0900

    コメントを修正

--- pages/_pointcloud/_season.vue
+++ pages/_pointcloud/_season.vue
@@ -382,7 +382,7 @@ export default {
       if (this.annotationData || this.listData) {
         this.drawerAlreadyOpened = true // あとで開く処理はスキップ
         this.clearSelectedAnnotation()
-        // nextTickを使わないと、vue-youtubeがリロードされないので注意（next/prevなどで遷移した時にそのまま動画が再生されてしまう）prev
+        // nextTickを使わないと、vue-youtubeがリロードされないので注意（next/prevなどで遷移した時にそのまま動画が再生されてしまう）
         this.$nextTick(() => {
           this.annotationData = e.target.data
           e.target.domElement.get(0).classList.add('highlighted')
