459ea85f0ae717b59b7960dc03abc6abee1262de
Naokazu Terada <naokazu.terada@gmail.com>
2021-10-05 20:48:29 +0900

    よくeslintのワーニングが出ていたので修正

--- data/gardens/joei_ji.js
+++ data/gardens/joei_ji.js
@@ -1,6 +1,7 @@
 export default {
   title: 'Jōei-ji Garden',
-  pointcloud: '/assets/pointclouds/Sessyutei_20211004_0.005.las_converted/metadata.json',
+  pointcloud:
+    '/assets/pointclouds/Sessyutei_20211004_0.005.las_converted/metadata.json',
   initCamera() {
     // viewer.fitToScreen()
     window.viewer.scene.view.position.set(46.97, 75.066, 25.272)
