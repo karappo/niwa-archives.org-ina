1a42fc1679465e85b79e8e7b1951ed18eb066f9d
Naokazu Terada <naokazu.terada@gmail.com>
2022-08-10 17:25:04 +0900

    - Add TODO comment - Rename constraints

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,8 @@
 const site_title = 'Incomplete Niwa Archives 終らない庭のアーカイヴ'
 const site_description = '2019年4月より始動した庭園アーカイヴ・プロジェクトは、現代のテクノロジーを駆使して、日本庭園の多様な側面をデータ化した新しい総合的アーカイヴを研究開発するものです。'
-// const site_url = 'https://special.ycam.jp/niwa/'
-const site_url = 'https://niwa.ycam.jp'
+// TODO: 環境変数から読み込むなどしてstagingなどにも対応する
+// const baseUrl = 'https://special.ycam.jp/niwa/'
+const baseUrl = 'https://niwa.ycam.jp'
 
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
@@ -24,14 +25,14 @@ export default {
       { hid: 'description', name: 'description', content: site_description },
       { hid: 'og:site_name', property: 'og:site_name', content: site_title },
       { hid: 'og:type', property: 'og:type', content: 'website' },
-      { hid: 'og:url', property: 'og:url', content: site_url },
+      { hid: 'og:url', property: 'og:url', content: baseUrl },
       { hid: 'og:title', property: 'og:title', content: site_title },
       { hid: 'og:description', property: 'og:description', content: site_description },
-      { hid: 'og:image', property: 'og:image', content: `${site_url}/share.jpg` },
+      { hid: 'og:image', property: 'og:image', content: `${baseUrl}/share.jpg` },
       { hid: 'twitter:title', property: 'twitter:title', content: site_title },
-      { hid: 'twitter:url', property: 'twitter:url', content: site_url },
+      { hid: 'twitter:url', property: 'twitter:url', content: baseUrl },
       { hid: 'twitter:description', property: 'twitter:description', content: site_description },
-      { hid: 'twitter:image', property: 'twitter:image', content: `${site_url}/share.jpg` },
+      { hid: 'twitter:image', property: 'twitter:image', content: `${baseUrl}/share.jpg` },
       { hid: 'twitter:card', property: 'twitter:card', content: 'summary_large_image' }
     ],
     link: [
