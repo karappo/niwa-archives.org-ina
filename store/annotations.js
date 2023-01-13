b62ce296dd82586a62f69271d0924e52c0471790
Naokazu Terada <naokazu.terada@gmail.com>
2023-01-13 15:45:58 +0900

    ページ追加時のstoreの処理を配列に一元化

--- store/annotations.js
+++ store/annotations.js
@@ -1,57 +1,52 @@
-export const state = () => ({
-  joeiJi: null,
-  murinAnSnow: null,
-  murinAnSummer: null,
-  murinAnWinter: null,
-  ryogenIn: null,
-  fugetsuRoFebruary: null,
-  fugetsuRoJuly: null
-})
-
-export const mutations = {
-  joeiJi(state, value) {
-    state.joeiJi = value
-  },
-  murinAnSnow(state, value) {
-    state.murinAnSnow = value
-  },
-  murinAnSummer(state, value) {
-    state.murinAnSummer = value
-  },
-  murinAnWinter(state, value) {
-    state.murinAnWinter = value
-  },
-  ryogenIn(state, value) {
-    state.ryogenIn = value
-  },
-  fugetsuRoFebruary(state, value) {
-    state.fugetsuRoFebruary = value
-  },
-  fugetsuRoJuly(state, value) {
-    state.fugetsuRoJuly = value
+// -------------------------------
+// ※ ページが増えたときはここに追加
+const pages = [
+  'joeiJi',
+  'murinAnSnow',
+  'murinAnSummer',
+  'murinAnWinter',
+  'ryogenIn',
+  'fugetsuRoFebruary',
+  'fugetsuRoJuly'
+]
+// -------------------------------
+const mapStates = () => {
+  const result = {}
+  for (const page of pages) {
+    result[page] = null
   }
+  return result
 }
-
-export const getters = {
-  joeiJi(state) {
-    return state.joeiJi
-  },
-  murinAnSnow(state) {
-    return state.murinAnSnow
-  },
-  murinAnSummer(state) {
-    return state.murinAnSummer
-  },
-  murinAnWinter(state) {
-    return state.murinAnWinter
-  },
-  ryogenIn(state) {
-    return state.ryogenIn
-  },
-  fugetsuRoFebruary(state) {
-    return state.fugetsuRoFebruary
-  },
-  fugetsuRoJuly(state) {
-    return state.fugetsuRoJuly
+const mapMutations = () => {
+  const result = {}
+  for (const page of pages) {
+    // eslint-disable-next-line
+    result[page] = new Function(
+      'state',
+      'val',
+      `state.${page} = val;`
+    )
   }
+  return result
+}
+const mapGetters = () => {
+  const result = {}
+  for (const page of pages) {
+    // eslint-disable-next-line
+    result[page] = new Function(
+      'state',
+      `return state.${page};`
+    )
+  }
+  return result
+}
+// -------------------------------
+export const state = () => ({
+  ...mapStates()
+})
+export const mutations = {
+  ...mapMutations()
+}
+export const getters = {
+  ...mapGetters()
 }
