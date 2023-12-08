eb7b983c63d48dafeb7c41dd697af59fbefba5bf
Naokazu Terada <naokazu.terada@gmail.com>
2023-12-08 10:00:03 +0900

    点群データ差し替え

--- data/gardens/fugetsu_ro-autumn.js
+++ data/gardens/fugetsu_ro-autumn.js
@@ -1,7 +1,7 @@
 export default {
   title: 'Fugetsuro - Autumn',
   pointcloud:
-    '/assets/pointclouds/20221227_FugetsuroPhotoRaserEdit0.01.las_converted/metadata.json',
+    '/assets/pointclouds/20231016_20221227_FugetsuroPhotoRaserEdit_10mm.las_converted/metadata.json',
   variations: ['spring', 'summer', 'autumn', 'winter'],
   initCamera() {
     // viewer.fitToScreen()
