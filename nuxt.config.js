f494cc6cbd88c0e41746e3ec60d62d8a55e5ee01
Naokazu Terada <naokazu.terada@gmail.com>
2024-08-27 15:43:11 +0900

    Fix warning

--- nuxt.config.js
+++ nuxt.config.js
@@ -122,9 +122,7 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-    transpile: [
-      'three'
-    ]
+    transpile: ['three']
   },
 
   // ローカルIPでデバッグしたい時に有効化
