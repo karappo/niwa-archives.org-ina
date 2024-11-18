8568941d846c515273480a4b37b1e0d0639e8315
Naokazu Terada <naokazu.terada@gmail.com>
2024-11-18 16:26:35 +0900

    deploy処理修正

--- deploy.sh
+++ deploy.sh
@@ -19,12 +19,12 @@ if yarn generate; then
   find ./dist/ -name '.nojekyll' -type f -delete
   if npm run rename; then
     # remove "DEP_XXX_RM "
-    ext=".temp_bakup" # extension of backup files which are created before replacement
-    find ./dist/ -name "*.htaccess" -exec sed -i$ext "s|#DEP_REMOTE_RM ||" {} \;
-    find ./dist/ -name "*.htaccess" -exec sed -i$ext "s|#DEP_"$DEP_ENV"_RM ||" {} \;
-    find ./dist/ -name "*.php" -exec sed -i$ext "s|//DEP_REMOTE_RM ||" {} \;
-    find ./dist/ -name "*.php" -exec sed -i$ext "s|//DEP_"$DEP_ENV"_RM ||" {} \;
-    find ./dist/ -name "*$ext" -exec rm {} \; # delete backup files
+    # ext=".temp_bakup" # extension of backup files which are created before replacement
+    # find ./dist/ -name "*.htaccess" -exec sed -i$ext "s|#DEP_REMOTE_RM ||" {} \;
+    # find ./dist/ -name "*.htaccess" -exec sed -i$ext "s|#DEP_"$DEP_ENV"_RM ||" {} \;
+    # find ./dist/ -name "*.php" -exec sed -i$ext "s|//DEP_REMOTE_RM ||" {} \;
+    # find ./dist/ -name "*.php" -exec sed -i$ext "s|//DEP_"$DEP_ENV"_RM ||" {} \;
+    # find ./dist/ -name "*$ext" -exec rm {} \; # delete backup files
     # Deploy
     rsync -aIzh --perms --owner --group --stats --delete -e "ssh -p $DEP_PORT" "--exclude-from=$PWD/.depignore" ./dist/ $DEP_TARGET
     npm run rename-back
