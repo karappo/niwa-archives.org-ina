8031b68ed1f9bf1dda6accb6880e8afddbb01ec9
Naokazu Terada <naokazu.terada@gmail.com>
2024-11-28 12:21:14 +0900

    カメラ位置調整

--- data/gardens/hosoo.js
+++ data/gardens/hosoo.js
@@ -3,8 +3,8 @@ export default {
   pointcloud: '20241126_Hosoo_PotreeConverted/metadata.json',
   initCamera() {
     // viewer.fitToScreen()
-    window.viewer.scene.view.position.set(30.522, -46.124, 4.345)
-    window.viewer.scene.view.lookAt(new THREE.Vector3(27.713, -45.114, 4.641))
+    window.viewer.scene.view.position.set(1.238, -43.205, 63.257)
+    window.viewer.scene.view.lookAt(new THREE.Vector3(1.168, -43.18, 60.258))
   },
   guidedTour: [],
   tours: []
