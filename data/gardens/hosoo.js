22c7ef9adeae3a8aea0cbed86e61833a35474164
Naokazu Terada <naokazu.terada@gmail.com>
2024-11-28 15:13:38 +0900

    全体が収まるように修正

--- data/gardens/hosoo.js
+++ data/gardens/hosoo.js
@@ -3,8 +3,8 @@ export default {
   pointcloud: '20241126_Hosoo_PotreeConverted/metadata.json',
   initCamera() {
     // viewer.fitToScreen()
-    window.viewer.scene.view.position.set(1.238, -43.205, 63.257)
-    window.viewer.scene.view.lookAt(new THREE.Vector3(1.238, -43.205, 60.257))
+    window.viewer.scene.view.position.set(1.868, -41.441, 90.144)
+    window.viewer.scene.view.lookAt(new THREE.Vector3(1.868, -41.441, 87.144))
   },
   guidedTour: [],
   tours: []
