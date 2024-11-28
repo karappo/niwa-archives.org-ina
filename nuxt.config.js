61614153340f65037b1bd6d13676a2a268b76f13
Naokazu Terada <naokazu.terada@gmail.com>
2024-11-28 12:30:02 +0900

    Add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,7 @@ export default {
       }
     ],
     link: [
-      // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
         type: 'text/css',
