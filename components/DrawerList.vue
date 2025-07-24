c3af1a0677d637593b4e30d95148989b85db1d57
Naokazu Terada <naokazu.terada@gmail.com>
2025-07-24 17:25:56 +0900

    pug to html

--- components/DrawerList.vue
+++ components/DrawerList.vue
@@ -1,30 +1,75 @@
-<template lang="pug">
-.content
-  header
-    h1(:data-name="title") {{ title }}
-    .filter(v-if="tags.length && !data.name.includes('Tour')")
-      FilterSelectBox(:options="tags" :value.sync="data.tagIndexStr")
-    DrawerCloseButton
-  article
-    template(v-if="data.name === 'Guided Tour'")
-      .description Guided Tourは、庭園をひとめぐりしながらINAの基本的な機能を把握することのできる自動モードです。
-      .bigBtn.guidedTour(@click="startTour('Guided Tour')") Start Tour
-    template(v-else-if="data.name === 'Ramble Tour'")
-      h3 Tour with Annotations
-      .description Ramble Tourは、全てのアノテーションをランダムにめぐってゆく自動モードです。Tour with Annotationsでは、各Annotationの説明を表示しながら進んでゆきます。
-      .bigBtn.rambleTour(@click="startTour('Ramble Tour with Annotations')") Start Tour
-      h3 Tour without Annotations
-      .description Ramble Tourは、全てのアノテーションをランダムにめぐってゆく自動モードです。Tour without Annotationsでは、各Annotationの説明を表示せず、園内を移動しつづけます。同時にSoundsを再生させることもできます。
-      .bigBtn.rambleTour(@click="startRambleTourWithoutAnnotations()") Start Tour
+<template>
+  <div class="content">
+    <header>
+      <h1 :data-name="title">{{ title }}</h1>
+      <div v-if="tags.length && !data.name.includes('Tour')" class="filter">
+        <FilterSelectBox
+          :options="tags"
+          :value.sync="data.tagIndexStr"
+        ></FilterSelectBox>
+      </div>
+      <DrawerCloseButton></DrawerCloseButton>
+    </header>
+    <article>
+      <template v-if="data.name === 'Guided Tour'">
+        <div class="description">
+          <!-- eslint-disable-next-line -->
+          Guided Tourは、庭園をひとめぐりしながらINAの基本的な機能を把握することのできる自動モードです。
+        </div>
+        <div class="bigBtn guidedTour" @click="startTour('Guided Tour')">
+          Start Tour
+        </div>
+      </template>
+      <template v-else-if="data.name === 'Ramble Tour'">
+        <h3>Tour with Annotations</h3>
+        <div class="description">
+          <!-- eslint-disable-next-line -->
+          Ramble Tourは、全てのアノテーションをランダムにめぐってゆく自動モードです。Tour with Annotationsでは、各Annotationの説明を表示しながら進んでゆきます。
+        </div>
+        <div
+          class="bigBtn rambleTour"
+          @click="startTour('Ramble Tour with Annotations')"
+        >
+          Start Tour
+        </div>
+        <h3>Tour without Annotations</h3>
+        <div class="description">
+          <!-- eslint-disable-next-line -->
+          Ramble Tourは、全てのアノテーションをランダムにめぐってゆく自動モードです。Tour without Annotationsでは、各Annotationの説明を表示せず、園内を移動しつづけます。同時にSoundsを再生させることもできます。
+        </div>
+        <div
+          class="bigBtn rambleTour"
+          @click="startRambleTourWithoutAnnotations()"
+        >
+          Start Tour
+        </div>
+      </template>
 
-    template(v-if="data.name !== 'Ramble Tour'")
-      .groups(v-if="data.name === 'Oral Archives'")
-        .group(v-for="(val, key) in this.groups")
-          .head
-            h5 {{ key }}
-            .thumb(:style="`background-image: url(${val[0].youtube.thumbnailUrl()});`")
-          AnnotationList(:list="filterByTag(val)" :typeVisibility="typeVisibility" :thin="true")
-      AnnotationList(v-else :list="filterByTag(data.list)" :typeVisibility="typeVisibility")
+      <template v-if="data.name !== 'Ramble Tour'">
+        <div v-if="data.name === 'Oral Archives'" class="groups">
+          <div v-for="(val, key) in groups" :key="key" class="group">
+            <div class="head">
+              <h5>{{ key }}</h5>
+              <div
+                class="thumb"
+                :style="`background-image: url(${val[0].youtube.thumbnailUrl()});`"
+              ></div>
+            </div>
+            <AnnotationList
+              :list="filterByTag(val)"
+              :typeVisibility="typeVisibility"
+              :thin="true"
+            ></AnnotationList>
+          </div>
+        </div>
+        <AnnotationList
+          v-else
+          :list="filterByTag(data.list)"
+          :typeVisibility="typeVisibility"
+        ></AnnotationList>
+      </template>
+    </article>
+  </div>
 </template>
 
 <style lang="sass" scoped>
