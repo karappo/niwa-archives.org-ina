561451cc92ab60a91aa587ec24d495f842fe8d7c
Naokazu Terada <naokazu.terada@gmail.com>
2025-07-31 14:54:43 +0900

    remove sass

--- components/TourIndicator.vue
+++ components/TourIndicator.vue
@@ -9,40 +9,50 @@
   </div>
 </template>
 
-<style lang="sass" scoped>
-@keyframes flash
-  0%
-    opacity: 1
-  100%
-    opacity: 0.25
-.tourModeIndicator
-  position: absolute
-  top: 25px
-  right: 25px
-  font-family: 'K2-v1-Bold'
-  font-size: 15px
-  color: white
-  display: flex
-  align-items: center
-  &:after
-    content: ''
-    display: inline-block
-    width: 18px
-    height: 18px
-    background-color: #00FDAF
-    border-radius: 50%
-    margin-left: 13px
-    animation: flash 0.5s ease-in-out infinite
-    animation-direction: alternate
-    @media only screen and (max-width: 428px)
-      margin-left: 0
-      margin-right: 10px
-  @media only screen and (max-width: 428px)
-    flex-direction: row-reverse
-    top: auto
-    right: auto
-    left: 15px
-    bottom: 10px
+<style scoped>
+@keyframes flash {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0.25;
+  }
+}
+.tourModeIndicator {
+  position: absolute;
+  top: 25px;
+  right: 25px;
+  font-family: 'K2-v1-Bold';
+  font-size: 15px;
+  color: white;
+  display: flex;
+  align-items: center;
+  &:after {
+    content: '';
+    display: inline-block;
+    width: 18px;
+    height: 18px;
+    background-color: #00fdaf;
+    border-radius: 50%;
+    margin-left: 13px;
+    animation: flash 0.5s ease-in-out infinite;
+    animation-direction: alternate;
+  }
+}
+@media only screen and (max-width: 428px) {
+  .tourModeIndicator {
+    flex-direction: row-reverse;
+    top: auto;
+    right: auto;
+    left: 15px;
+    bottom: 10px;
+
+    &:after {
+      margin-left: 0;
+      margin-right: 10px;
+    }
+  }
+}
 </style>
 
 <script>
