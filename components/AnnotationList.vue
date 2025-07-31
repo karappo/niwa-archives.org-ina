40227e2a24de5ab90ba99041913f528b39a7e60a
Naokazu Terada <naokazu.terada@gmail.com>
2025-07-31 17:23:40 +0900

    remove sass

--- components/AnnotationList.vue
+++ components/AnnotationList.vue
@@ -31,96 +31,122 @@
   </div>
 </template>
 
-<style lang="sass" scoped>
-.list
-  display: block
-  padding: 0
-  margin: 0
-  li + li
-    margin-top: 3px
-  &.thin
-    li
-      height: 45px
-  li
-    display: flex
-    padding: 0
-    margin: 0
-    background: #1A1A1A
-    color: #898989
-    cursor: pointer
-    font-size: 14px
-    height: 60px
-    transition: background 0.2s, color 0.2s
-    svg
-      path
-        transition: fill 0.2s
-    &:hover
-      background-color: darken(#1A1A1A, 5%)
-      color: white
-      svg
-        path
-          fill: white
+<style scoped>
+.list {
+  display: block;
+  padding: 0;
+  margin: 0;
+  li + li {
+    margin-top: 3px;
+  }
+  &.thin {
+    li {
+      height: 45px;
+    }
+  }
+  li {
+    display: flex;
+    padding: 0;
+    margin: 0;
+    background: #1a1a1a;
+    color: #898989;
+    cursor: pointer;
+    font-size: 14px;
+    height: 60px;
+    transition: background 0.2s, color 0.2s;
+    svg {
+      path {
+        transition: fill 0.2s;
+      }
+    }
+    &:hover {
+      background-color: darken(#1a1a1a, 5%);
+      color: white;
+      svg {
+        path {
+          fill: white;
+        }
+      }
+    }
 
-    .dotWrap
-      margin-right: 0
-      margin-left: auto
-      display: flex
-      align-items: center
-      --bg-color: transparent
-      &[data-category='Viewpoints/Still Images']
-        --bg-color: var(--color--still-images)
-      &[data-category='Viewpoints/Movies']
-        --bg-color: var(--color--movies)
-      &[data-category='Elements/Stones']
-        --bg-color: var(--color--stones)
-      &[data-category='Elements/Plants']
-        --bg-color: var(--color--plants)
-      &[data-category='Elements/Creatures']
-        --bg-color: var(--color--creatures)
-      &[data-category='Elements/Artifacts']
-        --bg-color: var(--color--artifacts)
-      &[data-category='Elements/DNA Data']
-        --bg-color: var(--color--dna-data)
-      &[data-category='Oral Archives']
-        --bg-color: var(--color--oral-archives)
-      .dot
-        display: inline-block
-        width: 6px
-        height: 6px
-        margin: 0 20px
-        background-color: var(--bg-color)
-        border-radius: 50%
+    .dotWrap {
+      margin-right: 0;
+      margin-left: auto;
+      display: flex;
+      align-items: center;
+      --bg-color: transparent;
+      &[data-category='Viewpoints/Still Images'] {
+        --bg-color: var(--color--still-images);
+      }
+      &[data-category='Viewpoints/Movies'] {
+        --bg-color: var(--color--movies);
+      }
+      &[data-category='Elements/Stones'] {
+        --bg-color: var(--color--stones);
+      }
+      &[data-category='Elements/Plants'] {
+        --bg-color: var(--color--plants);
+      }
+      &[data-category='Elements/Creatures'] {
+        --bg-color: var(--color--creatures);
+      }
+      &[data-category='Elements/Artifacts'] {
+        --bg-color: var(--color--artifacts);
+      }
+      &[data-category='Elements/DNA Data'] {
+        --bg-color: var(--color--dna-data);
+      }
+      &[data-category='Oral Archives'] {
+        --bg-color: var(--color--oral-archives);
+      }
+      .dot {
+        display: inline-block;
+        width: 6px;
+        height: 6px;
+        margin: 0 20px;
+        background-color: var(--bg-color);
+        border-radius: 50%;
+      }
+    }
 
-    .icon
-      margin-right: 0.5em
-    .thumb
-      width: 90px
-      height: 60px
-      background-size: 180% // 上下に黒帯が入った正方形なのでceoverにするとNG
-      background-position: center
-      background-color: #333
-      flex-shrink: 0
-    .index
-      margin-right: 0.75em
-      width: 30px
-      display: flex
-      align-items: center
-      justify-content: flex-end
-    .title
-      flex-shrink: 10
-      overflow: hidden
-      text-overflow: ellipsis
-      white-space: nowrap
-      display: flex
-      align-items: center
-    .type
-      flex-shrink: 0
-      width: 46px
-      display: flex
-      justify-content: center
-      align-items: center
-.empty
-  color: #898989
+    .icon {
+      margin-right: 0.5em;
+    }
+    .thumb {
+      width: 90px;
+      height: 60px;
+      background-size: 180%; /* 上下に黒帯が入った正方形なのでceoverにするとNG */
+      background-position: center;
+      background-color: #333;
+      flex-shrink: 0;
+    }
+    .index {
+      margin-right: 0.75em;
+      width: 30px;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+    }
+    .title {
+      flex-shrink: 10;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      display: flex;
+      align-items: center;
+    }
+    .type {
+      flex-shrink: 0;
+      width: 46px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+}
+.empty {
+  color: #898989;
+}
 </style>
 
 <script>
