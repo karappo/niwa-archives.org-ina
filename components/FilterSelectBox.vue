687050fc22e468f86a2fdca16cf30eeb16983eb1
Naokazu Terada <naokazu.terada@gmail.com>
2025-07-31 15:56:35 +0900

    remove sass

--- components/FilterSelectBox.vue
+++ components/FilterSelectBox.vue
@@ -21,42 +21,46 @@
   </div>
 </template>
 
-<style lang="sass" scoped>
-.selectBox
-  min-width: 156px
-  height: 40px
-  border: 0
-  margin-right: 15px
-  position: relative
-  flex-shrink: 0
-  select
-    cursor: pointer
-    -webkit-appearance: none
-    appearance: none
-    padding: 0
-    outline: 0
-    border: 0
-    width: 100%
-    height: 100%
-    padding: 7px 50px 8px 14px
-  .icon
-    pointer-events: none
-    position: absolute
-    top: 0
-    right: 0
-    height: 100%
-    width: 40px
-    border-left: 1px solid black
-    display: flex
-    justify-content: center
-    align-items: center
-  &:hover
-    select
-      color: white
-    .icon
-      svg
-        path
-          fill: white
+<style scoped>
+.selectBox {
+  min-width: 156px;
+  height: 40px;
+  border: 0;
+  margin-right: 15px;
+  position: relative;
+  flex-shrink: 0;
+  &:hover {
+    select {
+      color: white;
+    }
+    .icon svg path {
+      fill: white;
+    }
+  }
+  select {
+    cursor: pointer;
+    -webkit-appearance: none;
+    appearance: none;
+    padding: 0;
+    outline: 0;
+    border: 0;
+    width: 100%;
+    height: 100%;
+    padding: 7px 50px 8px 14px;
+  }
+  .icon {
+    pointer-events: none;
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 100%;
+    width: 40px;
+    border-left: 1px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
 </style>
 
 <script>
