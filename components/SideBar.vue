86f0331d548c49e6a8d87b513a9368cd01f2c901
Naokazu Terada <naokazu.terada@gmail.com>
2025-07-31 17:08:28 +0900

    remove sass

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <aside>
+  <aside class="sideBar">
     <div class="wrap">
       <div class="spHeader">
         <div class="closeBtn" @click="$emit('spClose')">
@@ -82,112 +82,153 @@
   </aside>
 </template>
 
-<style lang="sass" scoped>
-aside
-  background-color: #000
-  color: #898989
-  z-index: 1
-  border-left: 1px solid #3C3C3C
-  overflow-y: auto
-  @media only screen and (max-width: 428px)
-    height: 100%
-  > .wrap
-    padding-bottom: var(--main-min-height)
-.spHeader
-  display: none
-  @media only screen and (max-width: 749px)
-    --size: 54px
-    height: var(--size)
-    display: flex
-    align-items: center
-    justify-content: flex-end
-    position: sticky
-    top: 0
-    background: #000
-    z-index: 9
-  .closeBtn
-    width: var(--size)
-    height: var(--size)
-    display: flex
-    justify-content: center
-    align-items: center
-.backToTop
-  display: flex
-  align-items: center
-  font-family: 'K2-v1-Heavy'
-  font-weight: normal
-  font-size: 15px
-  padding: 18px 15px 18px 20px
-  cursor: pointer
-  span
-    font-size: 15px
-    letter-spacing: 0.01em
-    font-family: 'K2-v1-Bold'
-    color: #898989
-    margin-right: auto
-    transition: color 0.2s
-  svg
-    margin-left: auto
-    g
-      path
-        transition: fill 0.2s
-  &:hover
-    span
-      color: white
-    svg
-      g
-        path
-          fill: white
-section
-  padding: 11px 0
-  border-top: 1px solid #2C2C2C
+<style scoped>
+aside.sideBar {
+  background-color: #000;
+  color: #898989;
+  z-index: 1;
+  border-left: 1px solid #3c3c3c;
+  overflow-y: auto;
+  @media only screen and (max-width: 428px) {
+    height: 100%;
+  }
+  > .wrap {
+    padding-bottom: var(--main-min-height);
+  }
+}
+.spHeader {
+  display: none;
+  @media only screen and (max-width: 749px) {
+    --size: 54px;
+    height: var(--size);
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    position: sticky;
+    top: 0;
+    background: #000;
+    z-index: 9;
+  }
+  .closeBtn {
+    width: var(--size);
+    height: var(--size);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+.backToTop {
+  display: flex;
+  align-items: center;
+  font-family: 'K2-v1-Heavy';
+  font-weight: normal;
+  font-size: 15px;
+  padding: 18px 15px 18px 20px;
+  cursor: pointer;
+  span {
+    font-size: 15px;
+    letter-spacing: 0.01em;
+    font-family: 'K2-v1-Bold';
+    color: #898989;
+    margin-right: auto;
+    transition: color 0.2s;
+  }
+  svg {
+    margin-left: auto;
+    g {
+      path {
+        transition: fill 0.2s;
+      }
+    }
+  }
+  &:hover {
+    span {
+      color: white;
+    }
+    svg {
+      g {
+        path {
+          fill: white;
+        }
+      }
+    }
+  }
+}
+section {
+  padding: 11px 0;
+  border-top: 1px solid #2c2c2c;
   &.outlines,
-  &.tours
-    padding: 15px 0
-h2 + section
-  border-top: 0
-.variations
-  margin-left: 34px
-  margin-top: 10px
-  width: 121px
-  min-width: 0
-  /deep/ select
-    text-transform: capitalize
-h2
-  background-color: #171717
-  color: #898989
-  font-family: 'K2-v1-Heavy'
-  font-weight: normal
-  font-size: 14px
-  line-height: 2
-  letter-spacing: 0.01em
-  padding-top: 2px
-  padding-bottom: 2px
-  padding-left: 20px
-  margin: 0
-  /deep/ .linkCheck .btn
-    margin-left: 0
-h3
-  font-size: 13px
-  margin: 0
-  margin-bottom: 10px
-.btn
-  cursor: pointer
-  font-size: 13px
-  display: flex
-  align-items: center
-  &:hover
-    color: rgba(255,255,255,0.8)
-  &.current
-    color: white
-    pointer-events: none
+  &.tours {
+    padding: 15px 0;
+  }
+}
+h2 + section {
+  border-top: 0;
+}
+.variations {
+  margin-left: 34px;
+  margin-top: 10px;
+  width: 121px;
+  min-width: 0;
+  /deep/ select {
+    text-transform: capitalize;
+  }
+}
+h2 {
+  background-color: #171717;
+  color: #898989;
+  font-family: 'K2-v1-Heavy';
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 2;
+  letter-spacing: 0.01em;
+  padding-top: 2px;
+  padding-bottom: 2px;
+  padding-left: 20px;
+  margin: 0;
+}
+h3 {
+  font-size: 13px;
+  margin: 0;
+  margin-bottom: 10px;
+}
+.btn {
+  cursor: pointer;
+  font-size: 13px;
+  display: flex;
+  align-items: center;
+  &:hover {
+    color: rgba(255, 255, 255, 0.8);
+  }
+  &.current {
+    color: white;
+    pointer-events: none;
+  }
+}
 .btn + .btn,
-.linkCheck + .linkCheck
-  margin-top: 8px
-.disabled
-  &:hover
-    text-decoration: line-through
-    cursor: default
+.linkCheck + .linkCheck {
+  margin-top: 8px;
+}
+.disabled {
+  &:hover {
+    text-decoration: line-through;
+    cursor: default;
+  }
+}
+</style>
+
+<style>
+/* 下記のコードは本当はdeepでscopedの方の.linkCheckの中に入れたい */
+aside.sideBar {
+  .variations {
+    select {
+      text-transform: capitalize;
+    }
+  }
+  h2 .linkCheck .btn {
+    margin-left: 0;
+  }
+}
 </style>
 
 <script>
