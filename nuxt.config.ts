5590b89a7b72bb7329ba3f42adffd6074890b6b4
Naokazu Terada <naokazu.terada@gmail.com>
2025-09-04 16:52:44 +0900

    一旦ファイルをおいてみる

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  nitro: {
+    prerender: {
+      routes: [
+        '/fugetsu_ro-spring/',
+        '/fugetsu_ro-summer/',
+        '/fugetsu_ro-autumn/',
+        '/fugetsu_ro-winter/',
+        '/hosoo/',
+        '/joei_ji/',
+        '/murin_an-snow/',
+        '/murin_an-summer/',
+        '/murin_an-winter/',
+        '/ryogen_in/'
+      ]
+    }
+  }
+})
