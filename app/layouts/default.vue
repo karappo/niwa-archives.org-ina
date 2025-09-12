5727540447c0fb1ad354fa8c7a37a5b88d507f24
Naokazu Terada <naokazu.terada@gmail.com>
2025-09-12 15:47:57 +0900

    composition api 対応

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,26 +2,26 @@
   <NuxtPage />
 </template>
 
-<script>
-export default {
-  mounted() {
-    window.addEventListener('resize', this.resize)
-    this.resize()
-    document.documentElement.style.setProperty(
-      '--initial-vh',
-      `${window.innerHeight}px`
-    )
-  },
-  beforeUnmount() {
-    window.removeEventListener('resize', this.resize)
-  },
-  methods: {
-    resize() {
-      document.documentElement.style.setProperty(
-        '--vh',
-        `${window.innerHeight}px`
-      )
-    }
-  }
+<script setup>
+import { onMounted, onBeforeUnmount } from 'vue'
+
+const resize = () => {
+  document.documentElement.style.setProperty(
+    '--vh',
+    `${window.innerHeight}px`
+  )
 }
+
+onMounted(() => {
+  window.addEventListener('resize', resize)
+  resize()
+  document.documentElement.style.setProperty(
+    '--initial-vh',
+    `${window.innerHeight}px`
+  )
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', resize)
+})
 </script>
