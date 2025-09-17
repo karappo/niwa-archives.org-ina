fb6c42d459dcf846481af25047f201d0fb2999bb
Naokazu Terada <naokazu.terada@gmail.com>
2025-09-17 11:33:00 +0900

    'Viewpoints/'を含むものが全て同じ値なら、 Viewpointsの値もそれと同じにする。'Elements/'を含むものが全て同じ値なら、 Elementsの値もそれと同じにする。Annotations以外の値が全て同じなら、Annotationの値もそれと同じにする。

--- app/stores/main.js
+++ app/stores/main.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-
+import _uniq from 'lodash/uniq'
 export const useMainStore = defineStore('main', {
   state: () => ({
     lastUpdateDateTime: {}, // 各スプレッドシートの最終更新日時を保持
@@ -77,6 +77,25 @@ export const useMainStore = defineStore('main', {
           break
       }
       this.annotationVisibilities[key] = value
+
+      // 'Viewpoints/'を含むものが全て同じ値なら、 Viewpointsの値もそれと同じにする
+      const vKeys = Object.keys(this.annotationVisibilities).filter((key) => key.includes('Viewpoints/'))
+      const vValues = vKeys.map((key) => this.annotationVisibilities[key])
+      if (_uniq(vValues).length === 1) {
+        this.annotationVisibilities.Viewpoints = vValues[0]
+      }
+      // 'Elements/'を含むものが全て同じ値なら、 Elementsの値もそれと同じにする
+      const eKeys = Object.keys(this.annotationVisibilities).filter((key) => key.includes('Elements/'))
+      const eValues = eKeys.map((key) => this.annotationVisibilities[key])
+      if (_uniq(eValues).length === 1) {
+        this.annotationVisibilities.Elements = eValues[0]
+      }
+      // Annotations以外の値が全て同じなら、Annotationの値もそれと同じにする
+      const aKeys = Object.keys(this.annotationVisibilities).filter((key) => key !== 'Annotations')
+      const aValues = aKeys.map((key) => this.annotationVisibilities[key])
+      if (_uniq(aValues).length === 1) {
+        this.annotationVisibilities.Annotations = aValues[0]
+      }
     }
   }
 })
