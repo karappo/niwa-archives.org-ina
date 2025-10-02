b88308f5d933befe821f7eff78b7aa00497ff388
Naokazu Terada <naokazu.terada@gmail.com>
2025-10-02 10:16:22 +0900

    keyCodeを使わない。下部のコードも以前はモード切り替えで使っていたようだが、不要になったので削除
    84a6640

--- app/plugins/key-detection.client.ts
+++ app/plugins/key-detection.client.ts
@@ -4,42 +4,25 @@
 // onKeydown(e) {
 //   const key = $key(e)
 // }
-// TODO:
-// keyCodeはdeprecatedらしいので、使わない方法を模索したい
-// https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
-
 export default defineNuxtPlugin(() => {
   return {
     provide: {
       key: (e: KeyboardEvent) => {
         const key = e.key.toLowerCase()
+
+        // 日本語変換中は無視
+        if (e.isComposing) {
+          return null
+        }
+
         // OS間共通 ======================
-        if (e.keyCode === 13) { // 日本語変換時の確定が反応してしまわないようにkeyCodeで判定
+        if (key === 'enter') {
           return 'enter'
         }
 
         // Space キー
-        if (e.keyCode === 32) {
+        if (key === ' ' || e.code === 'Space') {
           return 'space'
-        }
-        
-        // Mac ==========================
-        if (navigator.platform.includes('Mac')) {
-          // Chrome以外 ------------------
-          if (!navigator.userAgent.includes('Chrome')) {
-            if (e.code?.toLowerCase() === 'space' && e.keyCode === 229 && !e.isComposing) {
-              return 'space'
-            }
-          }
-        }
-        // Windows ======================
-        else if (navigator.platform.includes('Win')) {
-          // Chrome ---------------------
-          if (navigator.userAgent.includes('Chrome')) {
-            if ((e.code && e.code.toLowerCase() === 'space') && e.keyCode === 229 && !e.isComposing) {
-              return 'space'
-            }
-          }
         }
 
         return key
